77c8a07d8a912407a735745c4bc5d40c1795ea40
frankkode <masasimpafra@gmail.com>
2020-02-14 00:27:09 +0100

    add arrowed host

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zambia-real-estate.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
